db482d87cad1cfa43ec651c82cb2a4dede3500ce
Damian Szmurło <48288156+ZiutekFronEnd@users.noreply.github.com>
2019-09-04 13:00:16 +0200

    Add files via upload
    added basic structures

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <navi />
+    <boxtarge />
+  </div>
+</template>
+
+<script>
+import navi from './components/navi.vue'
+import boxtarge from './components/boxtarge.vue'
+
+export default {
+  name: 'app',
+  components: {
+    navi,
+    boxtarge
+  }
+}
+</script>
+
+<style>
+*{
+    box-sizing: border-box;
+    padding: 0px;
+    margin: 0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+    background-color:#eceff1; 
+  color: #2c3e50;
+}
+body{
+   background-color:#eceff1; 
+}
+</style>
