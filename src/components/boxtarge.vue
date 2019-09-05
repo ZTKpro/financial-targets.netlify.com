b20f7d4bf01ffd669aca89b92226a98a1af3c45c
Damian <damianszmurlowork@gmail.com>
2019-09-05 10:53:44 +0200

    added financial target

--- src/components/boxtarge.vue
+++ src/components/boxtarge.vue
@@ -1,10 +1,66 @@
 <template>
-  <div  class="box">
+  <div class="box">
+    <h2>3 MONTHS</h2>
+
     <div id="box" class="box_item">
-      <h2>{{ dream1.name }}</h2>
-      <p class="item_cost">{{dream1.cost}}</p>
+      <h3>{{ months3.name }}</h3>
+      <p class="item_cost">{{months3.cost}}</p>
       <div class="box_progresbar">
-        <div class="bar"></div>
+        <div style="width:32%" class="bar"></div>
+      </div>
+    </div>
+
+    <div id="box" class="box_item">
+      <h3>{{ months3.name1 }}</h3>
+      <p class="item_cost">{{months3.cost1}}</p>
+      <div class="box_progresbar">
+        <div style="width:32%" class="bar"></div>
+      </div>
+    </div>
+
+    <div id="box" class="box_item">
+      <h3>{{ months3.name2 }}</h3>
+      <p class="item_cost">{{months3.cost2}}</p>
+      <div class="box_progresbar">
+        <div style="width:32%" class="bar"></div>
+      </div>
+    </div>
+
+    <h2>12 MONTHS</h2>
+
+    <div id="box" class="box_item">
+      <h3>{{ months12.name }}</h3>
+      <p class="item_cost">{{months12.cost}}</p>
+      <div class="box_progresbar">
+        <div style="width:62%" class="bar"></div>
+      </div>
+    </div>
+
+    <div id="box" class="box_item">
+      <h3>{{ months12.name1 }}</h3>
+      <p class="item_cost">{{months12.cost1}}</p>
+      <div class="box_progresbar">
+        <div style="width:62%" class="bar"></div>
+      </div>
+    </div>
+
+    <h2>3 YEARS</h2>
+
+    <div id="box" class="box_item">
+      <h3>{{ years3.name }}</h3>
+      <p class="item_cost">{{years3.cost}}</p>
+      <div class="box_progresbar">
+        <div style="width:2%" class="bar"></div>
+      </div>
+    </div>
+
+    <h2>6 YEARS</h2>
+
+    <div id="box" class="box_item">
+      <h3>{{ years6.name }}</h3>
+      <p class="item_cost">{{years6.cost}}</p>
+      <div class="box_progresbar">
+        <div style="width:2%" class="bar"></div>
       </div>
     </div>
   </div>
@@ -13,15 +69,41 @@
 export default {
   data() {
     return {
-      dream1: {
-        name: "ASG KIT",
-        cost: "1 286,00 zł"
+      months3: {
+
+        name: "KPP",
+        cost: "690,00 zł",
+        cost: "690,00 zł",
+
+        name1: "ASG KIT",
+        cost1: "1 286,00 zł",
+
+        name2: "OWD",
+        cost2: "1 290,00 zł"
+
+      },
+      months12: {
+
+        name: "MOTOBIKE",
+        cost: "11 550,00 zł",
+
+        name1: "DRIVING LICENSE",
+        cost1: "1 990,00 zł"
+
+      },
+      years3: {
+        name: "AUDI A5",
+        cost: "66 000,00 zł"
+      },
+      years6: {
+        name: "HOUSE",
+        cost: "300 000,00 zł"
       },
     };
   },
   methods: {
     appendDreams() {
-        console.log('aa')
+   
     }
   },
   created: function() {
@@ -30,8 +112,11 @@ export default {
 };
 </script>
 <style>
-.box{
-    padding:0 150px;
+h2 {
+  margin: 10px;
+}
+.box {
+  padding: 0 150px;
 }
 .box_item {
   height: 85px;
@@ -56,7 +141,6 @@ export default {
 }
 .box_progresbar > .bar {
   height: 100%;
-  width: 30%;
   background-color: #16cae6;
 }
 </style>
