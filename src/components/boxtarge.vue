c0787953ab5b8bcc4a3355fd9632d8c27baec024
Damian <damianszmurlowork@gmail.com>
2019-11-15 14:42:22 +0100

    transfered js array to json added function progressbar done

--- src/components/boxtarge.vue
+++ src/components/boxtarge.vue
@@ -1,140 +1,125 @@
 <template>
-  <div>
+<div>
     <div class="box">
-      <h2>months3</h2>
-      <div v-for="(item,item2,item3) in months3" class="box_item" :key="item.id">
-        <h3>{{item.nameTag}}</h3>
-        <p>{{item.mycost}}</p>
-        <p>{{item.toBuy}}</p>
-        <div class="box_progresbar">
-          <div class="bar" ></div>
+        <h2>months3</h2>
+        <div v-for="item in months3" class="box_item" :key="item.id">
+            <h3>{{item.nameTarget}}</h3>
+            <p id="itemMycost" class="hidden">{{item.mycost}}</p>
+            <p id="itemCost">{{item.cost}}</p>
+            <div class="box_progresbar">
+                <div class="bar"></div>
+            </div>
         </div>
-      </div>
-      <h2>months12</h2>
-      <div v-for="(item,item2,item3) in months12" class="box_item" :key="item.id">
-        <h3>{{item.nameTag}}</h3>
-        <p>{{item.mycost}}</p>
-        <p>{{item.cost}}</p>
-        <div class="box_progresbar">
-          <div class="bar"></div>
+        <h2>months12</h2>
+        <div v-for="item in months12" class="box_item" :key="item.id">
+            <h3>{{item.nameTarget}}</h3>
+            <p id="itemMycost" class="hidden">{{item.mycost}}</p>
+            <p id="itemCost">{{item.cost}}</p>
+            <div class="box_progresbar">
+                <div class="bar"></div>
+            </div>
         </div>
-      </div>
-      <h2>years3</h2>
-      <div v-for="(item,item2,item3) in years3" class="box_item" :key="item.id">
-        <h3>{{item.nameTag}}</h3>
-        <p>{{item.mycost}}</p>
-        <p>{{item.cost}}</p>
-        <div class="box_progresbar">
-          <div class="bar"></div>
+        <h2>years3</h2>
+        <div v-for="item in years3" class="box_item" :key="item.id">
+            <h3>{{item.nameTarget}}</h3>
+            <p id="itemMycost" class="hidden">{{item.mycost}}</p>
+            <p id="itemCost">{{item.cost}}</p>
+            <div class="box_progresbar">
+                <div class="bar"></div>
+            </div>
         </div>
-      </div>
-      <h2>years6</h2>
-      <div v-for="(item,item2,item3) in years6" class="box_item" :key="item.id">
-        <h3>{{item.nameTag}}</h3>
-        <p>{{item.mycost}}</p>
-        <p>{{item.cost}}</p>
-        <div class="box_progresbar">
-          <div class="bar"></div>
+        <h2>years6</h2>
+        <div v-for="item in years6" class="box_item" :key="item.id">
+            <h3>{{item.nameTarget}}</h3>
+            <p id="itemMycost" class="hidden">{{item.mycost}}</p>
+            <p id="itemCost">{{item.cost}}</p>
+            <div class="box_progresbar">
+                <div class="bar"></div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 </template>
+
 <script>
+import mont3 from "./jsonFiles/month3.json"
+import mont12 from "./jsonFiles/month12.json"
+import year3 from "./jsonFiles/years3.json"
+import year6 from "./jsonFiles/years6.json"
 export default {
-  data() {
-    return {
-      months3: {
-        item: {
-          nameTag: "KPP",
-          mycost: 25,
-          cost: 690,
-          toBuy: 0,
-        },
-        item2: {
-          nameTag: "ASG KIT",
-          mycost: 0,
-          cost: 1286,
-          toBuy: 0,
-        },
-        item3: {
-          nameTag: "OWD",
-          mycost: 0,
-          cost: 1290,
-          toBuy: 0,
+    data() {
+        return {
+            months3: mont3,
+            months12: mont12,
+            years3: year3,
+            years6: year6,
+        };
+    },
+    methods: {
+        progresBar: function () {
+            const itemMycost = document.querySelectorAll("#itemMycost")
+            const itemCost = document.querySelectorAll("#itemCost")
+            const bar = document.querySelectorAll(".bar")
+            for (let i = 0; i < itemCost.length; i++) {
+                let iC = itemCost[i].textContent
+                let iM = itemMycost[i].textContent
+                let bi = bar[i]
+                let alfa = Math.round(iM / iC * 100)
+                bi.style.width = alfa + "%"
+            }
         }
-      },
-      months12: {
-        item: {
-          nameTag: "DRIVING LICENSE",
-          mycost: 0,
-          cost: 1999
-        },
-        item1: {
-          nameTag: "MOTOBIKE",
-          mycost: "0",
-          cost: "11 550"
-        }
-      },
-      years3: {
-        item: {
-          nameTag: "AUDI A5",
-          mycost: "0",
-          cost: "66 000"
-        }
-      },
-      years6: {
-        item: {
-          nameTag: "HOUSE",
-          mycost: "0",
-          cost: "300 000"
-        }
-      }
-    };
-  },
-  methods: {
-  
-  },
+    },
+    mounted: function () {
+        this.progresBar()
+    }
 };
 </script>
-<style>
 
+<style>
 h2 {
-  margin: 10px;
+    margin: 10px;
 }
+
 .box {
-  padding: 0 150px;
+    padding: 0 150px;
 }
+
 .box_item {
-  height: 85px;
-  background-color: white;
-  border: 1px solid rgb(221, 221, 221);
-  padding: 25px;
-  margin: 10px auto;
-  border-radius: 5px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  overflow: hidden;
-  list-style: none;
+    height: 85px;
+    background-color: white;
+    border: 1px solid rgb(221, 221, 221);
+    padding: 25px;
+    margin: 10px auto;
+    border-radius: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    list-style: none;
 }
+
 .box_progresbar {
-  height: 5px;
-  width: 100%;
-  position: absolute;
-  left: 0px;
-  bottom: 0px;
-  background-color: rgb(192, 192, 192);
+    height: 5px;
+    width: 100%;
+    position: absolute;
+    left: 0px;
+    bottom: 0px;
+    background-color: rgb(192, 192, 192);
 }
-.box_progresbar > .bar {
-  height: 100%;
-  width: 0.45%;
-  background-color: #16cae6;
+
+.box_progresbar>.bar {
+    height: 100%;
+    background-color: #16cae6;
 }
+
+.hidden {
+    opacity: 0;
+}
+
 @media (max-width: 1200px) {
-  .box {
-    padding: 0 50px;
-  }
+    .box {
+        padding: 0 50px;
+    }
 }
 </style>
--- src/components/navi.vue
+++ src/components/navi.vue
@@ -1,43 +1,41 @@
 <template>
-  <div>
+<div>
     <nav class="nav">
-      <div class="nav_logo">
-        <h1>financal target</h1>
-      </div>
-      <div class="nav_plus">
-        <h2>+</h2>
-      </div>
+        <div class="nav_logo">
+            <h1>financal target</h1>
+        </div>
+        <div class="nav_plus">
+            <h2>+</h2>
+        </div>
     </nav>
-      <div class="nav_generator"></div>
-  </div>
+    <div class="nav_generator"></div>
+</div>
 </template>
 <script>
 </script>
 <style>
 .nav {
-  height: 85px;
-  width: 100%;
-  background-color: white;
-  border-bottom: 1px solid rgb(221, 221, 221);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 160px
+    height: 85px;
+    width: 100%;
+    background-color: white;
+    border-bottom: 1px solid rgb(221, 221, 221);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 160px
 }
-
 .nav_generator {
-  position: absolute;
-  top: 0px;
-  width: 0px;
-  height: 100vh;
-  width: 100vw;
-  background-color: red;
-  display: none;
+    position: absolute;
+    top: 0px;
+    width: 0px;
+    height: 100vh;
+    width: 100vw;
+    background-color: red;
+    display: none;
 }
-
 @media (max-width: 1200px) {
-  .nav {
-    padding: 0 50px;
-  }
+    .nav {
+        padding: 0 50px;
+    }
 }
 </style>
